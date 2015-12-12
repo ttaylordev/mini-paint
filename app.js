6faa708f0fd2d79a2f00e80b9eb0bc8239d15962
Tyson Taylor <mellowt@gmail.com>
2015-12-11 17:29:51 -0700

    major functionality breakthrough, cleaned up code

--- app.js
+++ app.js
@@ -1,23 +1,20 @@
-/*$(document).ready(function() {
-  $('.box').on('click', function() {
-  //(argument).jq command(function(){});
-    $(this).css('background-color',' white');
-});
-});*/
 // provide a backgraound image loading tool so that people can pixilate any image
-// provide a dynamic color selector instead of static colors. allow settings, 8-bit, 16-bit, 32-bit, or 64-bit color schemes
-// ideally anyone could essentially trace over their favorite characters and images to transform them into 8 bit characters.
+// ideally anyone could trace over their favorite characters and images to transform them into 8 bit characters.
 // maybe have a button that automatically 8-bits images.
-// square off the "pixels"
-// provide greyscale slider.
 // provide a way to snapshot images and save them, or upload them
 // social media interaction, a way to share it with friends.
-
+// cover or contain background image
+// custom color thingy--set defaults for those colors
+// original as defaults
+// click to drag
 $(document).ready(function() {
+  makeCanvas(50);
   var activeColor = '#ECFFF8';
-  var colors = ('white red green blue yellow brown #FFCB87');
   var mint = '#ECFFF8';
-
+  $("#colorPicker").on('change',function(e){
+    console.log(e);
+    activeColor = e.target.value;
+  });
   $('#red').on('click', function() {
     activeColor = 'red';
   });
@@ -39,50 +36,90 @@ $(document).ready(function() {
   $( "#white" ).click(function() {
     activeColor = '#ECFFF8';
   });
-  /*$( ".box" ).dblclick(function() {
-    activeColor = 'black';
-  });*/
-  $('#64x64').click(function() {
-    $('.64').hide();
-    $('.original').hide();
+
+  $('#8x8').click(function() {
+    $('div.box').remove();
+    makeCanvas(8);
   });
-  $('#128x128').click(function() {
-    $('.64').show();
-    $('.128').hide();
-    $('.original').hide();
+  $('#12x12').click(function() {
+    $('div.box').remove();
+    makeCanvas(12);
   });
-  $('#256x256').click(function() {
-    $('.64').show();
-    $('.128').show();
-    $('.256').hide();
-    $('.original').hide();
+  $('#16x16').click(function() {
+    $('div.box').remove();
+    makeCanvas(16);
+  });
+  $('#20x20').click(function() {
+    $('div.box').remove();
+    makeCanvas(20);
   });
   $('#original').click(function() {
-    $('.64').show();
-    $('.128').show();
-    $('.256').show();
-    $('.original').show();
-  });
+    $('div.box').remove();
+    makeCanvas(50);
 
+  });
+  var gridMe = (function() {
+    var count = 0;
+    return function(){
+      if (count % 2 === 0) {
+        $('div.box').css('outline', '1px solid gray');
+        count++;
+      } else {
+        $('div.box').css('outline', '0px');
+        count++;
+      }
+    };
+  })();
+
+  $('#showGrid').click(function() {
+    gridMe();
+  });
+  function makeCanvas(int) {
+    var sqNo = Math.pow(int,2);
+    for (i = 0;i<sqNo;i++){
+      var box = document.createElement("div");
+      box.className = "box";
+      $('.containerDiv').append(box);
+    }
+    $('.containerDiv').css('width', int*12+'px');
+
+  }
+  var containerDivClick = document.querySelector('.containerDiv');
+  containerDivClick.addEventListener('click', doSomething, false);
+
+  function doSomething(e) {
+    if (e.target !== e.currentTarget) {
+      var clickedItem = e.target.id;
+        $(e.target).css('background-color', activeColor);
+    }
+    e.stopPropagation();
+  }
+
+  $("#reset").on('click', function () {
+    $('.box').css('background-color', '#ECFFF8');
+  });
+});
+
+
+
+
+
+
+/*
   $('.box').on('click', function() {
     $(this).css('background-color', activeColor);
     $(this).removeClass(colors);
     $(this).addClass(activeColor);
-  });
+  });*/
+
   /*$('.box').dblclick(function() {
     $(this).removeClass(colors);
   });*/
-  $("#reset").on('click', function () {
-    $('.box').css('background-color', '#ECFFF8');
-    $('.box').removeClass(colors);
-    $('.box').addClass('white');
-    $('.64').show();
-    $('.128').show();
-    $('.256').show();
-    $('.original').show();
-  });
-});
 
+
+/*$( ".box" ).dblclick(function() {
+  activeColor = 'black';
+});*/
 
 
 
